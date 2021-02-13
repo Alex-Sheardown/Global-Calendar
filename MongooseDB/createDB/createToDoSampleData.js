fcb5effa65cbaaff9bbaa88bf99a86c07b2eb96a
nicholasbenyo <38173998+nicholasbenyo@users.noreply.github.com>
2021-02-13 15:31:42 -0800

    Updated table with users collection

--- MongooseDB/createDB/createToDoSampleData.js
+++ MongooseDB/createDB/createToDoSampleData.js
@@ -10,19 +10,19 @@ usersCollection.insert(
         startDate: "2020-01-01",
         endDate: "2021-01-31",
         isActive: false
-
     }
 )
 usersCollection.insert(
     {
         name: "Nick",
         userID: 1,
-        timeZone: "USA/Los_Angeles",
+        timeZone: "USA/Chicago",
         startDate: "2021-02-01",
         endDate: null,
         isActive: true
     }
 )
+
 usersCollection.insert(
     {
         name: "Emma",
@@ -31,7 +31,6 @@ usersCollection.insert(
         startDate: "2020-01-01",
         endDate: null,
         isActive: true
-
     }
 )
 usersCollection.insert(
@@ -42,121 +41,57 @@ usersCollection.insert(
         startDate: "2020-01-01",
         endDate: null,
         isActive: true
-
     }
 )
 
-db.createCollection('tasks')
-tasksCollection = db.getCollection("tasks")
-tasksCollection.remove({})
-tasksCollection.insert(
+db.createCollection('calendars')
+calendarsCollection = db.getCollection("calendars")
+calendarsCollection.remove({})
+calendarsCollection.insert(
     {
-        listId: 1,
-        tasks: [
+        calendarId: 1,
+        userId: 1,
+        name: "Nick's Seattle U Classes",
+        description: "For Seattle University Winter 2021 Quarter",
+        events: [
             {
-                description: "Pick up 2 cans of tomato",
-                taskId: 1,
-                shared: "N",
-                status: "I"
+                eventID: 1
             },
             {
-                description: "Pick up 2 onions",
-                taskId: 2,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Pick up 1 box of spagetti",
-                taskId: 3,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Pick up 1 (3 litter) Coke",
-                taskId: 4,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Cook recipe http://recipe.com/spagetti",
-                taskId: 5,
-                shared: "wife",
-                status: "I"
+                eventID: 3
             }
         ]
     }
 )
-tasksCollection.insert(
+calendarsCollection.insert(
     {
-        listId: 2,
+        calendarId: 3,
+        userId: 2,
+        name: "Emma's Seattle U Classes",
+        description: "For Seattle University Winter 2021 Quarter",
         tasks: [
             {
-                description: "Test drive a Porsche Boxter",
-                taskId: 1,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "To be gas consious, test drive a Tesla",
-                taskId: 2,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Ask your friend to give you a ride in his Lotus",
-                taskId: 3,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Ask to barrow the Mustang from my mom :-)",
-                taskId: 4,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Rent a Corvette",
-                taskId: 5,
-                shared: "N",
-                status: "I"
+                eventID: 2
             }
         ]
     }
 )
-tasksCollection.insert(
+calendarsCollection.insert(
     {
-        listId: 3,
+        calendarId: 2,
+        userId: 3,
+        name: "Alex's Seattle U Classes",
+        description: "For Seattle University Winter 2021 Quarter",
         tasks: [
             {
-                description: "Pick drawing boards from friend",
-                taskId: 1,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Buy pencils, pens, and notebooks from Staples",
-                taskId: 2,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Go to the MS Store to buy a new Surface 3",
-                taskId: 3,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Pick up a printer at Frys",
-                taskId: 4,
-                shared: "N",
-                status: "I"
-            },
-            {
-                description: "Get a couple of XBox Games to relax",
-                taskId: 5,
-                shared: "N",
-                status: "I"
+                eventID: 4
             }
         ]
     }
+)
+db.createCollection('calendars')
+calendarsCollection = db.getCollection("calendars")
+calendarsCollection.remove({})
+calendarsCollection.insert(
+    {}
 )
