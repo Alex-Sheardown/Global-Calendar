f51d63587ad7ba2a29a4dfdbc14b23585e42b7ee
Justin Thoreson <thoresonjd@gmail.com>
2021-02-16 19:55:11 -0800

    Added static routes for pages

--- MongooseDB/App.js
+++ MongooseDB/App.js
@@ -123,6 +123,9 @@ var App = /** @class */ (function () {
         this.expressApp.use('/', router);
         this.expressApp.use('/app/json/', express.static(__dirname + '/app/json'));
         this.expressApp.use('/', express.static(__dirname + '/pages'));
+        this.expressApp.use('/Week', express.static(__dirname + '/pages/Calendar/Week.html'));
+        this.expressApp.use('/Month', express.static(__dirname + '/pages/Calendar/Month.html'));
+        this.expressApp.use('/Year', express.static(__dirname + '/pages/Calendar/Year.html'));
     };
     return App;
 }());
