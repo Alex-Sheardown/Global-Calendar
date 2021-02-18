ef2a9b6e822535de7c28b1e304c374805aacd7f6
Nicholas Benyo <benyon@seattleu.edu>
2021-02-17 17:09:35 -0800

    Updated GlobalCal

--- MongooseDB/startdbClient.globalCalDB2.cmd
+++ MongooseDB/startdbClient.globalCalDB2.cmd
@@ -1,1 +1,2 @@
-mongo --port 3000 --authenticationDatabase admin
+mongo --port 3000 --authenticationDatabase admin --eval "load ('createDB/createGlobalCalSampleData.js'); load ('createDB/createAdminUser.js');"
+pause
