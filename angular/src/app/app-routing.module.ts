1f1bb407da3c63838f1668bcc96efb33fc16838a
Emma T <tamuraemma@gmail.com>
2021-03-07 20:10:11 -0800

    Comment out <app-user> from app component and can get user displayed doing localhost4200/user.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent } //localhost4200/user to see it.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
