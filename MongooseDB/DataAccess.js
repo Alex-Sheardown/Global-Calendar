03174f8bed3f209037eafa75d86927703dd02b9b
Emma T <tamuraemma@gmail.com>
2021-03-13 16:03:33 -0800

    add and edited out Emma mongodb

--- MongooseDB/DataAccess.js
+++ MongooseDB/DataAccess.js
@@ -17,8 +17,9 @@ var DataAccess = /** @class */ (function () {
         this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
         return this.mongooseInstance;
     };
+    //static DB_CONNECTION_STRING:string = 'mongodb+srv://MrBob:<password>@cluster0.eiq1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     //static DB_CONNECTION_STRING:string = 'mongodb+srv://benyon:Seahawkssuck2021@calendardb.uhgwf.mongodb.net/globalCalendar?retryWrites=true&w=majority';
-    DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:3000/globalCalendar?authSource=admin';
+    DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:3000/globalCalendar?authSource=admin'; //in my local computer
     return DataAccess;
 }());
 exports.DataAccess = DataAccess;
