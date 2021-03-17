06e386b3b292f91dfdd466525b03c8f657ffe0a2
Nicholas Benyo <benyon@seattleu.edu>
2021-03-16 20:58:39 -0700

    add oauth

--- MongooseDB/googleOauth2.ts
+++ MongooseDB/googleOauth2.ts
@@ -1,6 +1,6 @@
 class googleOauth2 {
-    static id: string = '';
-    static secret:string = '';
+    static id: string = '986997073455-g8tgvpmfrm6jkhss1o8l4kn8l0m8i8pc.apps.googleusercontent.com';
+    static secret:string = '-36xjI8E9B5iVNHg0jp8uu-z';
 
 }
 export default googleOauth2;
